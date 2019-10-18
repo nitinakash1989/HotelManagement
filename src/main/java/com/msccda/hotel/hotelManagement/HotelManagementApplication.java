7cd0f2188d1446fa166e3f470a5428a0f850a2c2
nitinakash1989 <nitin.akash1989@gmail.com>
2019-10-18 18:56:39 -0300

    Testing script for findAllCustomer fucntion

--- src/main/java/com/msccda/hotel/hotelManagement/HotelManagementApplication.java
+++ src/main/java/com/msccda/hotel/hotelManagement/HotelManagementApplication.java
@@ -1,13 +1,29 @@
 package com.msccda.hotel.hotelManagement;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HotelManagementApplication {
+import com.msccda.hotel.hotelManagement.dao.CustomerDao;
 
+@SpringBootApplication
+public class HotelManagementApplication implements CommandLineRunner{
+	
+	@Autowired
+	CustomerDao customerDao;
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	public static void main(String[] args) {
 		SpringApplication.run(HotelManagementApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("\nAll customers---->{}\n", customerDao.findAllCustomer());
 	}
 
 }
