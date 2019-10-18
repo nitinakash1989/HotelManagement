d0446856b62f169dc69d6619b004391f540ac4f3
nitinakash1989 <nitin.akash1989@gmail.com>
2019-10-18 18:07:55 -0300

    Added customer entity

---
+++ src/main/java/com/msccda/hotel/hotelManagement/entity/Customer.java
@@ -0,0 +1,116 @@
+package com.msccda.hotel.hotelManagement.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+	@Id
+	private int customerId;
+	private String firstName;
+	private String lastName;
+	private String streetNumber;
+	private String streetName;
+	private String city;
+	private String province; 
+	private String postalCode;
+	private String country;
+	private String phoneNumber;
+	private String email;
+	
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+	 
+	
+	public Customer(int customerId, String firstName, String lastName, String streetNumber, String streetName,
+			String city, String province, String postalCode, String country, String phoneNumber, String email) {
+		super();
+		this.customerId = customerId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.streetNumber = streetNumber;
+		this.streetName = streetName;
+		this.city = city;
+		this.province = province;
+		this.postalCode = postalCode;
+		this.country = country;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+	public void setStreetNumber(String streetNumber) {
+		this.streetNumber = streetNumber;
+	}
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Person [customerId=" + customerId + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", streetNumber=" + streetNumber + ", streetName=" + streetName + ", city=" + city + ", province="
+				+ province + ", postalCode=" + postalCode + ", country=" + country + ", phoneNumber=" + phoneNumber
+				+ ", email=" + email + "]";
+	}
+}
